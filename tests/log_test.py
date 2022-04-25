4af709e71b66cf0e4ba68c9946315a4106bc3ab6
PanchV <32369823+PanchV@users.noreply.github.com>
2022-04-25 18:14:17 -0400

    updating requirements

--- tests/log_test.py
+++ tests/log_test.py
@@ -1,11 +1,20 @@
 import os
+import json
+import app.config
+
+# def test_logfile_misc_debug():
+#     """ check if misc_debug.log exists """
+#     test_path = os.path.dirname(os.path.abspath(__file__))
+#     log_dir = os.path.join(test_path, '../app/logs')
+#     filepath = os.path.join(log_dir, "misc_debug.log")
+#     assert os.path.isfile(filepath) #== False
 
 def test_logfile_misc_debug():
     """ check if misc_debug.log exists """
-    test_path = os.path.dirname(os.path.abspath(__file__))
-    log_dir = os.path.join(test_path, '../app/logs')
+    log_dir = app.config.Config.LOG_DIR
     filepath = os.path.join(log_dir, "misc_debug.log")
-    assert os.path.isfile(filepath) #== False
+    assert os.path.isfile(filepath)
+
 
 def test_logfile_request():
     """ check if misc_debug.log exists """
