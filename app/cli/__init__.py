223ab61b5a451bc594c3df350d7ca16fae913953
PanchV <32369823+PanchV@users.noreply.github.com>
2022-04-27 15:02:58 -0400

    CLI Updates

--- app/cli/__init__.py
+++ app/cli/__init__.py
@@ -1,16 +1,14 @@
 import click
 from flask.cli import with_appcontext
 from app.db import db
+import os
 
 @click.command(name='create-log-folder')
 @with_appcontext
 def create_log_folder():
     """ create log directory """
-    # get root directory of project
     root = os.path.dirname(os.path.abspath(__file__))
-    # set the name of the apps log folder to logs
     logdir = os.path.join(root, '../logs')
-    # make a directory if it doesn't exist
     if not os.path.exists(logdir):
         os.mkdir(logdir)
 
@@ -19,11 +17,8 @@ def create_log_folder():
 @with_appcontext
 def create_database():
     """ create database directory """
-    # get root directory of project
     root = os.path.dirname(os.path.abspath(__file__))
-    # set the name of the apps log folder to logs
     dbdir = os.path.join(root, '../../database')
-    # make a directory if it doesn't exist
     if not os.path.exists(dbdir):
         os.mkdir(dbdir)
-    db.create_all()
+    #db.create_all()
