5bd2359c22da292f48bb1f561a44083a1f2e9487
PanchV <32369823+PanchV@users.noreply.github.com>
2022-04-29 15:50:46 -0400

    Additional Tests - 2

--- tests/dashboard_test.py
+++ tests/dashboard_test.py
@@ -2,15 +2,10 @@ from app.db.models import User
 from app import db
 
 def _test_dashboard(application, add_db_user_fixture):
-    """ GET to dashboard as logged in user """
-    # pylint: disable=redefined-outer-name
     user = add_db_user_fixture
 
     with application.test_client(user=user) as client:
         resp = client.get('dashboard')
 
-    # check if successful at getting /dashboard
     assert resp.status_code == 200
-    assert b'<h2>Dashboard</h2>' in resp.data
-    assert b'<p>Welcome: testuser@test.com</p>' in resp.data
-    assert b'<h2>Browse: Your Songs</h2>' in resp.data
+    assert b'Dashboard' in resp.data
