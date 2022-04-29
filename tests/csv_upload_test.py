2b2ae2253f5579d89a853ac372a89a99fe6f1e65
PanchV <32369823+PanchV@users.noreply.github.com>
2022-04-29 16:01:40 -0400

    CSV Upload Adjustments

--- tests/csv_upload_test.py
+++ tests/csv_upload_test.py
@@ -1,9 +1,6 @@
-def _test_upload_songs(application, add_db_user_fixture):
-    """ unit test file upload """
-
-    # from https://blog.entirely.digital/flask-pytest-testing-uploads/
+def _upload_songs_test(application, add_db_user_fixture):
     root = config.Config.BASE_DIR
-    filename = 'sample.csv'
+    filename = 'songs.csv'
     filepath = root + '/../tests/' + filename
     user = add_db_user_fixture
 
@@ -11,12 +8,23 @@ def _test_upload_songs(application, add_db_user_fixture):
     upload_file = os.path.join(upload_folder, filename)
     if os.path.exists(upload_file):
         os.remove(upload_file)
-
     assert resp.status_code == 302
-
-    assert db.session.query(Song).count() == 1 # pylint: disable=no-member
+    assert db.session.query(Song).count() == 1
     song1 = Song.query.filter_by(artist="Eminem").first()
     assert song1.title == "Without Me"
-
     assert os.path.exists(upload_file)
     os.remove(upload_file)
+
+
+def _upload_songs_deny_test(client):
+    resp = client.get('songs/upload', follow_redirects=True)
+    assert b'Login Failed' in resp.data
+    root = config.Config.BASE_DIR
+    filename = 'songs.csv'
+    filepath = root + '/../tests/' + filename
+    with open(filepath, 'rb') as file:
+        data = {
+            'file': (file, filename),
+        }
+        resp = client.post('songs/upload', follow_redirects=True, data=data)
+    assert resp.status_code == 200
